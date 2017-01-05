fd72b4cbf82126da3e5d53dfa1b7657ec16967b5
Patrick Pangilinan <patrick.pangilinan@healthway.com.ph>
2017-01-05 16:34:10 +0800

    First Upload

--- MvcCRUD/App_Start/BundleConfig.cs
+++ MvcCRUD/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MvcCRUD
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.yeti.css",
+                      "~/Content/bootstrap.comso.css",
                       "~/Content/site.css"));
         }
     }
